7d405751524cfedbb40ff167d813fa4b5da0b23c
Ankit Das <58646861+ankit0812-hue@users.noreply.github.com>
2020-04-20 13:46:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+connect.then((db) =>{
+    console.log('Connected correctly to server');
+    Dishes.create({
+        name: 'Burger',
+        description: 'Food'
+    })
+    .then((dish) =>{
+        console.log(dish);
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set: {
+                description: 'Updated Food'
+            }
+        },{
+            new: true
+        }).exec();
+    })
+    .then((dish) =>{
+        console.log(dish);
+        dish.comments.push({
+            rating: 5,
+            comment: 'This is awesome',
+            author: 'Ankit'
+        });
+        return dish.save();
+    })
+    .then((dish) =>{
+        console.log(dish);
+        return Dishes.remove({});
+
+    })
+    .then(() =>{
+        return mongoose.connection.close();
+    })
+    .catch((err) =>{
+        console.log(err);
+    });
+
+});
---
+++ models/dishes.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const commentSchema = new schema({
+    rating: {
+        type:Number,
+        min:1,
+        max:5,
+        required:true
+    },
+    comment: {
+        type:String,
+        required:true
+    },
+    author: {
+        type:String,
+        required:true
+    }
+
+},{
+    timestamps:true
+});
+const dishSchema = new schema({
+    name:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    comments:[commentSchema]
+},
+{
+    timestamps: true
+}
+);
+
+var dishes = mongoose.model('dish',dishSchema);
+module.exports = dishes;
